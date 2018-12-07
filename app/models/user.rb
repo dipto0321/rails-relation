eb710454b10c34e30fa3e56076a8d76d6b26cdc6
dipto0321 <diptokmk47@gmail.com>
2018-12-07 09:24:23 +0600

    Playing with associations
        * One to Many (has_many)
        * One to One (has_one)
        * Many to Many (has_and_belongs_to_many)
        * Has Many Through
        * Has One  Through

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_one  :address, dependent: :destroy
+  has_and_belongs_to_many :events
+  has_many :games, through: :enrollments
+  has_one :purse
+  has_one :payment_history, through: :purse
+end
